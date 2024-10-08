18f938beec50ee4c318e64cc88299acd1bdefe7a
MajaGrys <majagrys@wp.pl>
2024-10-08 14:36:39 +0200

    Added paintings controller

---
+++ src/paintings/paintings.controller.ts
@@ -0,0 +1,30 @@
+import { Controller, Get, Post, Patch, Delete, Param, Body, Query} from '@nestjs/common';
+
+@Controller('paintings')
+export class PaintingsController {
+
+    @Get() // GET /paintings
+    getPaintings() {
+        return 'all paintings';
+    }
+
+    @Get(':id') // GET /paintings/:id
+    getOnePainting(@Param('id') id: string) {
+        return 'one painting';
+    }
+
+    @Post() // POST /paintings
+    createPainting(@Body() painting: {}) {
+        return 'create';
+    }
+
+    @Patch(':id') // PATCH /paintings/:id
+    updatePainting(@Param('id') id: string, @Body() paintingUpdate: {}) {
+        return 'update';
+    }
+
+    @Delete(':id') // DELETE /paintings/:id
+    deletePainting(@Param('id') id: string) {
+        return 'delete';
+    }
+}
