db8dc788f1ba5f45ee60ca04ce2b90588d5db5b1
MajaGrys <majagrys@wp.pl>
2024-10-09 11:47:42 +0200

    Added paintings service

--- src/paintings/paintings.service.ts
+++ src/paintings/paintings.service.ts
@@ -2,4 +2,63 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class PaintingsService {
+    private paintings = [
+        {
+            "id": 1,
+            "title": "Starry Night",
+            "author": "Vincent Van Gogh",
+            "year": 1889,
+        },
+        {
+            "id": 2,
+            "title": "Girl with a Pearl Earring",
+            "author": "Johannes Vermeer",
+            "year": 1665,
+        }
+    ]
+
+    getPaintings(author?) {
+        if (author) {
+            return this.paintings.filter(painting => painting.author == author)
+        };
+
+        return this.paintings;
+    }
+
+    getOnePainting(id: number) {
+        const painting = this.paintings.find(painting => painting.id === id);
+
+        return painting;
+    }
+
+    createPainting(painting: { title: string, author: string, year: number }) {
+        const paintingsByHighestId = [...this.paintings].sort((a,b) => b.id - a.id);
+        const newPainting = {
+            id: paintingsByHighestId[0].id + 1, // generates new highest id
+            ...painting
+        };
+
+        this.paintings.push(newPainting);
+        return newPainting;
+    }
+
+    updatePainting(id: number, updatedPainting: { title?: string, author?: string, year?: number }) {
+        this.paintings = this.paintings.map(painting => {
+            if (painting.id === id) {
+                return {...painting, ...updatedPainting} // updatedPainting will override only the properties it contains
+            };
+
+            return painting;
+        })
+
+        return this.getOnePainting(id);
+    }
+
+    deletePainting(id: number) {
+        const removedPainting = this.getOnePainting(id);
+
+        this.paintings = this.paintings.filter(painting => painting.id !== id);
+
+        return removedPainting;
+    }
 }
