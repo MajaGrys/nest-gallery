27b419d3e2db2422d82bb8aeb8f8aeeb8961380b
MajaGrys <majagrys@wp.pl>
2024-10-09 11:48:00 +0200

    Connected service to the controller

--- src/paintings/paintings.controller.ts
+++ src/paintings/paintings.controller.ts
@@ -1,30 +1,32 @@
 import { Controller, Get, Post, Patch, Delete, Param, Body, Query} from '@nestjs/common';
+import { PaintingsService } from './paintings.service';
 
 @Controller('paintings')
 export class PaintingsController {
+    constructor(private readonly paintingsService: PaintingsService) {}
 
-    @Get() // GET /paintings or e.g. /paintings?author=vangogh&year=1888
-    getPaintings(@Query('author') author?: string) {
-        return 'all paintings';
+    @Get() // GET /paintings or e.g. /paintings?author=Vincent Van Gogh
+    getPaintings(@Query('author') author?) {
+        return this.paintingsService.getPaintings(author);
     }
 
     @Get(':id') // GET /paintings/:id
     getOnePainting(@Param('id') id: string) {
-        return 'one painting';
+        return this.paintingsService.getOnePainting(+id);
     }
 
     @Post() // POST /paintings
-    createPainting(@Body() painting: { }) {
-        return 'create';
+    createPainting(@Body() painting: { title: string, author: string, year: number }) {
+        return this.paintingsService.createPainting(painting);
     }
 
     @Patch(':id') // PATCH /paintings/:id
-    updatePainting(@Param('id') id: string, @Body() paintingUpdate: { }) {
-        return 'update';
+    updatePainting(@Param('id') id: string, @Body() paintingUpdate: { title?: string, author?: string, year?: number }) {
+        return this.paintingsService.updatePainting(+id, paintingUpdate);
     }
 
     @Delete(':id') // DELETE /paintings/:id
     deletePainting(@Param('id') id: string) {
-        return 'delete';
+        return this.paintingsService.deletePainting(+id);
     }
 }
